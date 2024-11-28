0a094142c4d855198b11e230a18964a1e3d557a0
HumbertoSN <a620163215@my.uvm.edu.mx>
2024-11-28 16:54:42 -0600

    Prueba

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app=express()
+const Note = require('./models/note')
+
 
 app.use(express.json())
 app.use(cors())
@@ -16,30 +19,16 @@ const requestLogger = (req, res, next) => {
 
 app.use(requestLogger);
 
-let notes = [
-    {
-        id: 1,
-        content:'HTML is easy',
-        important: true
-    },
-    {
-        id: 2,
-        content:'Browser can execute only Javascript',
-        important: false
-    },
-    {
-        id: 3,
-        content:'GET and POST are the most important methods of HTTP Protocol',
-        important: true
-    }
-]
+let notes = []
 
 app.get('/',(request,response) => {
     response.send('<h1>API REST FROM NOTES</H1>')
 })
 
 app.get('/api/notes',(request,response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id',(request,response) => {
@@ -95,7 +84,7 @@ app.put('/api/notes/:id',(request,response) => {
     response.json(updateNote)
 })
 
-const PORT= process.env.PORT || 3001
+const PORT= process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server express running on port ${PORT}`);
 })
